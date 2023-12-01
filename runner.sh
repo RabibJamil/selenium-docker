37ef47eceb67ae3fa9408d8d92f3948caf20869f
Rabib Jamil <rabibjamilqa@gmail.com>
2023-12-01 11:09:13 -0500

    runner file update

--- runner.sh
+++ runner.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-#-------------------------------------------------------------------
-#  This script expects the following environment variables
-#     HUB_HOST
-#     BROWSER
-#     THREAD_COUNT
-#     TEST_SUITE
-#-------------------------------------------------------------------
-
-# Let's print what we have received
 echo "-------------------------------------------"
 echo "HUB_HOST      : ${HUB_HOST:-hub}"
 echo "BROWSER       : ${BROWSER:-chrome}"
@@ -16,7 +5,6 @@ echo "THREAD_COUNT  : ${THREAD_COUNT:-1}"
 echo "TEST_SUITE    : ${TEST_SUITE}"
 echo "-------------------------------------------"
 
-# Do not start the tests immediately. Hub has to be ready with browser nodes
 echo "Checking if hub is ready..!"
 count=0
 while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
@@ -31,10 +19,9 @@ do
   sleep 1
 done
 
-# At this point, selenium grid should be up!
 echo "Selenium Grid is up and running. Running the test...."
 
-# Start the java command
+
 java -cp 'libs/*' \
      -Dselenium.grid.enabled=true \
      -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
