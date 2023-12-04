5a79bc9e0add311564c0dab72596d8c6bcf9126e
Rabib Jamil <rabibjamilqa@gmail.com>
2023-12-04 10:00:22 -0500

    runner file update

--- runner.sh
+++ runner.sh
@@ -5,18 +5,19 @@ echo "THREAD_COUNT  : ${THREAD_COUNT:-1}"
 echo "TEST_SUITE    : ${TEST_SUITE}"
 echo "-------------------------------------------"
 echo "Checking if hub is ready..!"
-count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
-  count=$((count+1))
-  echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
-  then
-      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-      exit 1
-  fi
-  sleep 1
-done
+#count=0
+#while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
+#do
+#  count=$((count+1))
+#  echo "Attempt: ${count}"
+#  if [ "$count" -ge 30 ]
+#  then
+#      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+#      exit 1
+#  fi
+#  sleep 1
+#done
+sleep 20
 echo "Selenium Grid is up and running. Running the test...."
 java -cp 'libs/*' \
      -Dselenium.grid.enabled=true \
