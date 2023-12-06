33505cd84721a272dd5bb698974b943051b801f3
RabibJamil <54376645+RabibJamil@users.noreply.github.com>
2023-12-06 10:02:42 -0500

    update

--- runner.sh
+++ runner.sh
@@ -7,18 +7,18 @@ echo "TEST_SUITE    : ${TEST_SUITE}"
 echo "-------------------------------------------"
 # Do not start the tests immediately. Hub has to be ready with browser nodes
 echo "Checking if hub is ready..!"
-#count=0
-#while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-#do
-#  count=$((count+1))
-#  echo "Attempt: ${count}"
-#  if [ "$count" -ge 30 ]
-#  then
-#      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-#      exit 1
-#  fi
-#  sleep 1
-#done
+count=0
+while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
+do
+  count=$((count+1))
+  echo "Attempt: ${count}"
+  if [ "$count" -ge 30 ]
+  then
+      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+      exit 1
+  fi
+  sleep 1
+done
 # At this point, selenium grid should be up!
 echo "Selenium Grid is up and running. Running the test...."
 # Start the java command
